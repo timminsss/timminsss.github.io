cfb659dad7d6eee15d528f31d23fa8b02f3ca670
Shelley Timmins <shelley.timmins@hotmail.co.uk>
2023-07-04 20:24:06 +1000

    swift project added and tech stack hover over images added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,6 +6,7 @@ import RPS from '../../assets/portfolio3.png'
 import Connect4 from '../../assets/portfolio4.png'
 import Calc from '../../assets/portfolio5.png'
 import Todo from '../../assets/portfolio6.png'
+import Timer from '../../assets/portfolio7.png'
 
 const portfolioData = [
   {
@@ -13,42 +14,75 @@ const portfolioData = [
     image: Groupappo,
     title: 'Groupappo',
     github: 'https://github.com/timminsss/groupappo',
-    demo: 'https://groupappo-app.herokuapp.com/'
+    demo: 'https://groupappo-app.herokuapp.com/',
+    tech: [
+      'Ruby on Rails',
+      'Javascript',
+      'APIs',
+      'Bootstrap',
+      'Postgres'
+    ]
   },
   {
     id: 2,
     image: Bookappo,
     title: 'Bookappo',
     github: 'https://github.com/timminsss/groupappo-marketplace',
-    demo: 'https://groupappo-marketplace-app.herokuapp.com/'
+    demo: 'https://groupappo-marketplace-app.herokuapp.com/',
+    tech: [
+      'Ruby on Rails',
+      'Javascript',
+      'Bootstrap'
+    ]
   },
   {
     id: 3,
     image: RPS,
     title: 'Rock, Paper, Scissors',
     github: 'https://github.com/timminsss/rock-paper-scissors',
-    demo: 'https://timminsss.github.io/rock-paper-scissors/'
+    demo: 'https://timminsss.github.io/rock-paper-scissors/',
+    tech: [
+      'React'
+    ]
   },
   {
     id: 4,
     image: Connect4,
     title: 'Connect4',
     github: 'https://github.com/timminsss/connect-four',
-    demo: 'https://timminsss.github.io/connect-four/'
+    demo: 'https://timminsss.github.io/connect-four/',
+    tech: [
+      'React'
+    ]
   },
   {
     id: 5,
     image: Calc,
     title: 'Calculator',
     github: 'https://github.com/timminsss/calculator-react-app',
-    demo: 'https://timminsss.github.io/calculator-react-app/'
+    demo: 'https://timminsss.github.io/calculator-react-app/',
+    tech: [
+      'React'
+    ]
   },
   {
     id: 6,
     image: Todo,
     title: 'To Do List',
     github: 'https://github.com/timminsss/to-do-list',
-    demo: 'https://timminsss.github.io/to-do-list/'
+    demo: 'https://timminsss.github.io/to-do-list/',
+    tech: [
+      'Typescript'
+    ]
+  },
+  {
+    id: 7,
+    image: Timer,
+    title: 'iOS Timer and Stopwatch',
+    github: 'https://github.com/timminsss/stopwatch-swift',
+    tech: [
+      'Swift'
+    ]
   }
 ];
 
@@ -59,16 +93,24 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
         {
-          portfolioData.map(({id, image, title, github, demo}) => {
+          portfolioData.map(({id, image, title, github, demo, tech}) => {
             return (
               <article key={id} className="portfolio__item">
                 <div className="portfolio__item-image">
-                  <img src={image} alt={title} />
+                  <img className="portfolio__item-image" src={image} alt={title} />
+                  <ul className="portfolio__tech-stack">
+                      {tech.map(stack => (
+                        <li key={stack}>{stack}</li>
+                      ))}
+                    </ul>
                 </div>
                   <h3>{title}</h3>
                   <div className="portfolio__item-cta">
                     <a href={github} className='btn' target='_blank'>Github</a>
-                    <a href={demo} className='btn btn-primary portfolio__item-cta' target='_blank'>Live Demo</a>
+                    {demo && (
+                      <a href={demo} className='btn btn-primary portfolio__item-cta' target='_blank'>Live Demo</a>
+                    )
+                    }
                   </div>
               </article>
             )
